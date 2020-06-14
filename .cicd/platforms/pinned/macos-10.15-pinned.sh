c300349582dcb033805f908cb1d3f6fd31cd6d71
Emory Barlow <emory@parrinc2.com>
2020-06-14 18:27:41 -0400

    Add sbin to path for rabbitmq

--- .cicd/platforms/pinned/macos-10.15-pinned.sh
+++ .cicd/platforms/pinned/macos-10.15-pinned.sh
@@ -23,3 +23,5 @@ cd ..
 sudo rm -rf boost_1_72_0.tar.bz2 boost_1_72_0
 # install nvm for ship_test
 cd ~ && brew install nvm && mkdir -p ~/.nvm && echo "export NVM_DIR=$HOME/.nvm" >> ~/.bash_profile && echo 'source $(brew --prefix nvm)/nvm.sh' >> ~/.bash_profile && cat ~/.bash_profile && source ~/.bash_profile && echo $NVM_DIR && nvm install --lts=dubnium
+# Add sbin to path for rabbitmq
+echo "export PATH=$PATH:/usr/local/sbin" >> ~/.bash_profile
